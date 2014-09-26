d2a2c85c5166a0bb0fa40f037af2b7773a89f9cc
polamjag <s@polamjag.info>
2014-09-26 23:40:02 +0900

    Revert "add Resolv.getname support"
    This reverts commit effabf54602eae67d5ffca6e817345dff4240c10.

--- main.rb
+++ main.rb
@@ -2,7 +2,6 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'json'
-require 'resolv'
 
 INFO_TO_DISPLAY = %w{
   REMOTE_ADDR REMOTE_HOST REQUEST_METHOD REQUEST_URI
@@ -12,25 +11,11 @@ INFO_TO_DISPLAY = %w{
   HTTP_X_FORWARDED_FOR HTTP_X_REAL_IP
 }
 
-def safe_getname(ipaddr, is_format_embedded = true)
-  unless ipaddr.nil? || ipaddr.empty?
-    if is_format_embedded
-      "#{ipaddr} (#{Resolv.getname ipaddr})"
-    else
-      Resolv.getname ipaddr
-    end
-  else
-    ""
-  end
-end
-
 get '/' do
   @info = INFO_TO_DISPLAY.map do |i|
     [i, Rack::Utils.escape_html(@env[i])]
   end
   @info = Hash[*@info.flatten]
-  @info['HTTP_X_FORWARDED_FOR'] = safe_getname @info['HTTP_X_FORWARDED_FOR'], true
-  @info['HTTP_X_REAL_IP']       = safe_getname @info['HTTP_X_REAL_IP'], true
   @info = @info.map do |k, v|
     "<dt>#{k}</dt><dd>#{v}</dd>"
   end
@@ -43,18 +28,13 @@ get '/json' do
   @info = INFO_TO_DISPLAY.map do |i|
     [i, @env[i]]
   end
-  @info.push ['HTTP_X_FORWARDED_FOR_NAME', safe_getname(@env['HTTP_X_FORWARDED_FOR'], false)]
-  @info.push ['HTTP_X_REAL_IP_NAME', safe_getname(@env['HTTP_X_REAL_IP'], false)]
   Hash[*@info.flatten].to_json
 end
 
 get '/csv' do
   content_type 'text/plain'
-  @info = INFO_TO_DISPLAY.map { |i|
+  INFO_TO_DISPLAY.map { |i|
     "\"#{i}\",\"#{@env[i]}\"\r\n"
-  }
-  @info.push "\"HTTP_X_FORWARDED_FOR_NAME\",\"#{safe_getname @env['HTTP_X_FORWARDED_FOR'], false}\"\r\n"
-  @info.push "\"HTTP_X_REAL_IP_NAME\",\"#{safe_getname @env['HTTP_X_REAL_IP'], false}\"\r\n"
-  @info.join
+  }.join
 end
 
